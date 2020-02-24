0cabb7142628d1a402cca4e15544775e027d8bbe
hermelap <if708598@iteso.mx>
2020-02-24 08:51:24 -0600

    Laboratorio 2

---
+++ datos.py
@@ -0,0 +1,7 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: datos.py - datos generales para uso en el proyecto
+# -- mantiene: Hermela Peña
+# -- repositorio: https://github.com/hermelap/LAB_2_HPH
+# -- ------------------------------------------------------------------------------------ -- #
---
+++ main.py
@@ -0,0 +1,7 @@
+
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: main.py - flujo principal del proyecto
+# -- mantiene: Hermela Peña
+# -- repositorio: https://github.com/hermelap/LAB_2_HPH
+# -- ------------------------------------------------------------------------------------ -- #
